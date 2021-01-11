1c96628b47d517a72ea79a828076df4f56e07695
giuseppetrivi <giuseppetrivisanogt@gmail.com>
2021-01-12 00:20:07 +0100

    Menu e intestazione

--- contatti.php
+++ contatti.php
@@ -11,7 +11,7 @@
   <div class="overlay"></div>
   <div class="menu">
     <h1>MENU</h1>
-    <div class="menu-option clearfix">
+    <div class="menu-option selected clearfix">
       <i class="fas fa-user-circle"></i>
       <h6>Profilo</h6>
     </div>
@@ -24,12 +24,24 @@
       <h6>Utenti</h6>
     </div>
     <div class="menu-option clearfix">
+      <i class="fas fa-chart-pie"></i>
+      <h6>Statistiche</h6>
+    </div>
+    <div class="menu-option clearfix">
       <i class="fas fa-cog"></i>
       <h6>Impostazioni</h6>
     </div>
   </div>
 
   <!-- (menu-opener) con intestazione -->
+  <div class="top-page">
+    <div class="menu-opener">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <h1 class="top-titolo">IF TEAM</h1>
+  </div>
 
   <!-- container (con titolo sezione e il resto) -->
 </body>
